392a240f420728ba19ccfef0e204c3ed5e16e73c
danieljesus <daniel.j.rodrigues06@gmail.com>
2020-09-04 16:25:12 -0300

    First commit

---
+++ app/src/main/java/com/danieljrodrigues/sweepstakesapp/MainActivity.java
@@ -0,0 +1,23 @@
+package com.danieljrodrigues.sweepstakesapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view) {
+        TextView text = findViewById(R.id.numberDrawn);
+        int value = new Random().nextInt(11);
+        text.setText("O número sorteado é: " + value);
+    }
+}
